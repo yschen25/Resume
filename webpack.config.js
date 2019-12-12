0afe4a7485bc41296408141870d70d1f3825af31
Ka <yschen25@gmail.com>
2019-12-13 00:26:29 +0800

    resume commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     entry: {
         index: ['./js/index.js']
     },
     output: {
-        filename: 'bundle.js',
+        filename: './dist/bundle.js',
         path: path.resolve('./'),
-    }
+    },
+    module: {
+        rules: [{
+            test: /\.(scss|sass)$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "sass-loader"
+            ]
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./dist/[name].css"
+        })
+    ]
 };
