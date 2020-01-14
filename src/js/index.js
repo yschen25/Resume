dc5b86f04b9ca8ba8164a09b5c604d591de53be9
kaka <kaka369@pchomepay.com.tw>
2020-01-14 14:47:50 +0800

    resume

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    $('.loading').hide();
+    // $('.loading').hide();
 
     // Loading
     loading();
@@ -16,52 +16,46 @@ $(function () {
         }, 3000);
     }
 
-    var flag = true;
-    $('.page').scroll(function () {
-        if (!flag) return;
-        var height = $(this).height();
-        var scrollTop = $(this).scrollTop();
-        var scrollHeight = $(this).prop('scrollHeight');
-        console.log(scrollTop);
-        if (height + scrollTop === scrollHeight) {
-            flag = false;
-            $(this).animate({scrollTop: scrollTop - 5}, 100, 'swing', function () {
-                flag = true;
-            });
-            moveToNextPage();
-            addColorOnMenu();
-        }
-        if (scrollTop === 0) {
-            flag = false;
-            $(this).animate({scrollTop: 5}, 100, 'swing', function () {
-                flag = true;
-            });
-            moveToPreviousPage();
-            addColorOnMenu();
-        }
-    });
-
-    // Change menu's color when change page
-    $('.tag').on('click', function () {
-        $('.tag').removeClass('menuColor');
-        $(this).addClass('menuColor');
-    });
-
-    // Scroll
-    // $(window).bind('mousewheel', function (e) {
-    //
-    //     if (e.originalEvent.wheelDelta / 120 > 0) {
-    //         console.log('scroll up');
-    //
-    //         moveToPreviousPage();
-    //         addColorOnMenu();
-    //     } else {
-    //         console.log('scroll down');
-    //
+    // var flag = true;
+    // $('.page').scroll(function () {
+    //     if (!flag) return;
+    //     var height = $(this).height();
+    //     var scrollTop = $(this).scrollTop();
+    //     var scrollHeight = $(this).prop('scrollHeight');
+    //     console.log(scrollTop);
+    //     if (height + scrollTop === scrollHeight) {
+    //         flag = false;
+    //         $(this).animate({scrollTop: scrollTop - 5}, 100, 'swing', function () {
+    //             flag = true;
+    //         });
     //         moveToNextPage();
     //         addColorOnMenu();
     //     }
+    //     if (scrollTop === 0) {
+    //         flag = false;
+    //         $(this).animate({scrollTop: 5}, 100, 'swing', function () {
+    //             flag = true;
+    //         });
+    //         moveToPreviousPage();
+    //         addColorOnMenu();
+    //     }
     // });
+
+    // Scroll
+    $(window).bind('mousewheel', function (e) {
+
+        if (e.originalEvent.wheelDelta / 120 > 0) {
+            console.log('scroll up');
+
+            moveToPreviousPage();
+            addColorOnMenu();
+        } else {
+            console.log('scroll down');
+
+            moveToNextPage();
+            addColorOnMenu();
+        }
+    });
 
     // Change page when click specific keys
     $(window).on("keydown", function (e) {
@@ -81,23 +75,21 @@ $(function () {
         }
     });
 
+    // Change menu's color when change page
+    $('.tag').on('click', function () {
+        $('.tag').removeClass('menuColor');
+        $(this).addClass('menuColor');
+    });
+
     function moveToPreviousPage() {
         let tag = parseInt($("input[name=tag]:checked").val().slice(3, 5));
 
-        console.log('up', tag);
-
+        // First page
         if (tag === 1) {
             return;
         }
 
-        if (tag === 7) {
-            tag = tag - 3;
-        }
-
         let prevTag = $('#tag' + tag).prev();
-
-        console.log('prevTag', prevTag);
-
         $(prevTag).click();
     }
 
@@ -107,19 +99,11 @@ $(function () {
         console.log('down', tag);
 
         // Last page
-        if (tag === 10) {
+        if (tag === 5) {
             return;
         }
 
-        // Lead to autobiography
-        if (tag === 4) {
-            tag = tag + 2;
-        }
-
         let nextTag = $('#tag' + tag).next();
-
-        console.log('nextTag', nextTag);
-
         $(nextTag).click();
     }
 
@@ -127,12 +111,8 @@ $(function () {
         let tag = parseInt($("input[name=tag]:checked").val().slice(3, 5));
 
         $('.tag').removeClass('menuColor');
-        if (tag === 10 || tag === 9 || tag === 8) {
-            $('.tag7').addClass('menuColor');
-        } else {
-            $('.tag' + tag).addClass('menuColor');
-        }
-    }
+        $('.tag' + tag).addClass('menuColor');
+    };
 
     // About Me - contact info
     $('.imgWrapper').hover(function () {
